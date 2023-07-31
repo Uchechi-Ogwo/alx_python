dcda4b847d83dc985d1e6025c67879199359bd36
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-07-31 21:57:04 +0100

    task 2

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,12 +1,12 @@
 import random
 
-# Generate a random signed number between -100 and 100 (inclusive)
-number = random.randint(-100, 100)
+number = random.randint(-10, 10)
 
-# Check if the number is positive, negative, or zero, and print the result
 if number > 0:
-    print("{} is positive".format(number))
+  print("{} is positive".format(number))
+  
 elif number == 0:
-    print("{} is zero".format(number))
+  print("{} is zero".format(number))
+  
 else:
-    print("{} is negative".format(number))
+  print("{} is negative".format(number))
