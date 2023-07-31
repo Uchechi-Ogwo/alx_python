dbcaf5ce59dba279b29e1757e44b51b1d38c40cd
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-07-31 21:36:05 +0100

    task 6

---
+++ python-functions/6-password.py
@@ -0,0 +1,18 @@
+def validate_password(password):
+    # Check length
+    if len(password) < 8:
+        return False
+
+    # Check for at least one uppercase letter, one lowercase letter, and one digit
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    if not (has_upper and has_lower and has_digit):
+        return False
+
+    # Check for spaces
+    if ' ' in password:
+        return False
+
+    return True
