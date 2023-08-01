17c4fdbfe9a484e2c159b08c9893cddbb823a56d
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-01 19:12:01 +0100

    task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Call the add function with a and b as arguments and store the result in a variable called result
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
