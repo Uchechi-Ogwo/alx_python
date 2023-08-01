b146e52f9112f86a1583f49c0cc16d04379839f8
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-01 23:15:36 +0100

    task 0 to 9

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,12 @@
 import sys
 
-def print_arguments():
-    num_args = len(sys.argv) - 1
-    args_word = "argument" if num_args == 1 else "arguments"
-    ending = ":" if num_args > 0 else "."
+def main():
+  args = sys.argv[1:]
+  number_args = len(args)
+  
+  print("{} arguement{}:".format(number_args, 's' if number_args != 1 else ''))
+  for i, arg in enumerate(args, 1):
+    print("{}: {}".format(i, arg))
     
-    print(f"Number of argument(s): {num_args}, followed by {args_word}{ending}\n")
-    
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
-
-if __name__ == "__main__":
-    print_arguments()
+  if __name__ == "__main__":
+    main()
