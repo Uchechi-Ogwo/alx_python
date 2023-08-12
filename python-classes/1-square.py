3f1d63eeebc84ce2af23c04e2e5d2c3e2beed0c1
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-12 01:28:29 +0100

    task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,13 @@
+"""Write a class Square that defines a square by
+Private instance attribute size and Instantation with optional size 
+  """
 class Square:
-    def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+  """This method initialize a square method and check
+  if the value is of type int or less than 0
+  """
+  def __init__(self, size=0):
+      if type(size) != int:
+         raise TypeError("size must be an integer")
+      if size < 0:
+         raise ValueError("size must be >= 0")
+      self.__size = size 
