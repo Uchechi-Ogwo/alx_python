b32feeedb58acc460563d7a2338cc84c68b1f05d
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-12 01:36:29 +0100

    task 2 to 6

---
+++ python-classes/4-main.py
@@ -0,0 +1,16 @@
+quare = __import__('4-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
