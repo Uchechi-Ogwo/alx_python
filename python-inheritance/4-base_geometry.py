abe250de95e5f25b0809981eb091fd9b118f81d8
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-14 21:12:41 +0100

    task 4

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,27 +1,9 @@
-""" 
-The module of a class name BaseGeometry
-"""
+#!/usr/bin/python3
+"""BaseGeometry class Module"""
 
 
-class BaseGeometery:
-    """ 
-    This method check if a class is an instance of the define object
-    example 1 is an instance of the class int
-    """
-
-    def __dir__(cls) -> None:
-        """ 
-        The function overide the default __dir__ function
-        """
-        attrib = super().__dir__()
-        n_attri = []
-        for attr in attrib:
-            if attr != '__init_subclass__':
-                n_attri.append(attr)
-        return n_attri
-
+class BaseGeometry():
+    """BaseGeometry class"""
     def area(self):
-        """ 
-        This function raise an error if the area method is not define
-        """
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
