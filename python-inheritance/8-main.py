b1355a361a717bbdaac00f3be717cb2cf2927a05
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-15 15:27:19 +0100

    task 8

--- python-inheritance/8-main.py
+++ python-inheritance/8-main.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Square class Module"""
-Rectangle = __import__("9-rectangle").Rectangle
+
+"""importing Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""Write a class square that inherites from rectangle"""
 
 
 class Square(Rectangle):
-    """Square class"""
+    """A subclass of Rectangle"""
     def __init__(self, size):
-        """Method for initializing a square"""
+        """initialize private attribute size and validate it"""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-        """Method that returns area of a square"""
+        """returns area of a square"""
         return self.__size ** 2
 
     def __str__(self):
-        """Method that returns a string"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """Return and print the square description"""
+        return str("[Square] {:d}/{:d}".format(self.__size, self.__size))
