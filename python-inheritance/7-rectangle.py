887b35a8d4c3c36d8a874df1aab500ec7a5f70f8
Uchechi-Ogwo <ogwouchechi@gmail.com>
2023-08-15 20:44:19 +0100

    task 7

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,21 +1,42 @@
-#!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
+""" 
+Write a class Rectangle that inherits from BaseGeometry(5-base_geometry.py)
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
+class BaseGeometryMetaClass(type):
+    """ 
+    A metaclass for Base Geomerty
+    """
+    def __dir__(cls) -> None:
+        """ 
+        A function define to remove the __init_subclass__ from dir
+        """
+        return [attribute for attribute in super().__dir__ if attribute != '__init_subclass__']
+
+
+class Rectangle(BaseGeometry, metaclass=BaseGeometryMetaClass):
+    """ 
+    Write a class rectangle that inherits from BaseGeometry (5-base_geometry.py).
+    """
+
     def __init__(self, width, height):
-        """Initilize rectangle method"""
-        self.integer_validator("width", width)
+        """ 
+        Initialaizotio function for base geometry
+        """
+        width = BaseGeometry.integer_validator(self, "width", width)
+        height = BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
-        self.integer_validator("height", height)
         self.__height = height
 
-    def area(self):
-        """Method that returns area of rectangle"""
-        return self.__width * self.__height
-
     def __str__(self):
-        """Returns a string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """ 
+        This method convert the object to a readable string
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """ 
+        This method return the result of width * height
+        """
+        return self.__width * self.__height
